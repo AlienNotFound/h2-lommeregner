9cec8cd13570ebfe43040eba71d8c4d0d123b9a9
aliennotfound <adamdreioe@gmail.com>
2022-08-26 21:22:49 +0200

    JavaScript calc done

--- script.js
+++ script.js
@@ -1,68 +1,112 @@
 var outputBox = document.getElementById("output")
-outputBox.value = "0"
+outputBox.innerText = "0"
 
 var numButtons = document.getElementsByClassName("numb")
 
 for (let i = 1; i < numButtons.length + 1; i++) {
     document.getElementById(i).onclick = function () {
-        if(outputBox.value === "0") {
-            outputBox.value = "" + i + ""
+        if(outputBox.innerText === "0") {
+            outputBox.innerText = "" + i + ""
         } else {
-            outputBox.value += "" + i + ""
+            outputBox.innerText += "" + i + ""
         }
     }
 }
 
-document.addEventListener('keydown', (event) => {
-    var name = event.key;
-    var code = event.code;
-    if (name === 0) {
-        console.log(name)
+document.getElementById("0").onclick = function () {
+    if(outputBox.innerText !== "0") {
+        outputBox.innerText += 0
     }
+}
+
+document.addEventListener('keydown', (event) => {
+    var keyName = event.key;
+    var keyCode = event.code;
+    var p = ["NumpadAdd", "NumpadSubtract", "NumpadDivide", "NumpadMultiply"]
+
+    for (let i = 0; i < 10; i++) {
+        if (parseInt(keyName) === i) {
+            if(outputBox.innerText === "0") {
+                outputBox.innerText = keyName
+            } else {
+                outputBox.innerText += keyName
+            }
+        }
+    }
+
+    for(let i = 0; i < p.length; i++) {
+        if (keyCode === p[i]) {
+            if(outputBox.innerText !== "0") {
+                outputBox.innerText += keyName
+            }
+        }
+    }
+        switch (keyCode) {
+            case "Backspace":
+                bckspcInput();
+                break;
+
+            case "Delete":
+                clearInput();
+                break;
+
+            case "Enter":
+                equals();
+                break;
+
+            case "NumpadEnter":
+                equals();
+                break;
+        }
+
+    console.log(keyCode)
 })
 
-document.getElementById("0").onclick = function () {
-    if(outputBox.value !== "0") {
-        outputBox.value += 0
+function clearInput() {
+    outputBox.innerText = "0"
+    document.getElementById("xtra").innerText = ""
+}
+
+function bckspcInput() {
+    outputBox.innerText = outputBox.innerText.slice(0, -1)
+    document.getElementById("xtra").innerText = ""
+    if (outputBox.innerText <= 0) {
+        outputBox.innerText = "0"
     }
 }
 
-function plus() {
-    outputBox.value += "+"
-}
-
-function minus() {
-    outputBox.value += "-"
-}
-
-function times() {
-    outputBox.value += "*"
-}
-function divide() {
-    outputBox.value += "/"
+function operator(x) {
+    outputBox.innerText += x
 }
 
 function equals() {
-    if(outputBox.value.includes("+"))
+    if(outputBox.innerText.includes("+"))
     {
-        word = outputBox.value.split('+')
-        outputBox.value = parseInt(word[0]) + parseInt(word[1])
+        word = outputBox.innerText.split('+')
+        outputBox.innerText = parseInt(word[0]) + parseInt(word[1])
 
         console.log(parseInt(word[0]) + parseInt(word[1]))
-    } else if(outputBox.value.includes("-")){
-        word = outputBox.value.split('-')
-        outputBox.value = parseInt(word[0]) - parseInt(word[1])
+    } else if(outputBox.innerText.includes("-")){
+        word = outputBox.innerText.split('-')
+        outputBox.innerText = parseInt(word[0]) - parseInt(word[1])
 
         console.log(parseInt(word[0]) - parseInt(word[1]))
-    } else if(outputBox.value.includes("*")){
-        word = outputBox.value.split('*')
-        outputBox.value = parseInt(word[0]) * parseInt(word[1])
+    } else if(outputBox.innerText.includes("*")){
+        word = outputBox.innerText.split('*')
+        outputBox.innerText = parseInt(word[0]) * parseInt(word[1])
 
         console.log(parseInt(word[0]) * parseInt(word[1]))
-    } else if(outputBox.value.includes("/")){
-        word = outputBox.value.split('/')
-        outputBox.value = parseInt(word[0]) / parseInt(word[1])
+    } else if(outputBox.innerText.includes("/")){
+        word = outputBox.innerText.split('/')
+        outputBox.innerText = parseInt(word[0]) / parseInt(word[1])
 
         console.log(parseInt(word[0]) / parseInt(word[1]))
     }
+
+    if(outputBox.innerText === "69" || outputBox.innerText === "420") {
+        document.getElementById("xtra").innerText = "Nice."
+    } else {
+        document.getElementById("xtra").innerText = ""
+    }
 }
+
