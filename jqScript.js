cd0cb9308473a673a8b291e18072b02bcc759685
aliennotfound <adamdreioe@gmail.com>
2022-08-30 14:07:56 +0200

    More jq work

--- jqScript.js
+++ jqScript.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 
     for(let i = 0; i < numButtons.length; i++) {
         $("#" + i + "").click(function () {
+
             if(opbStr === "0") {
                 opbStr = i.toString()
                 outputBox.text(opbStr)
@@ -16,4 +17,21 @@ $(document).ready(function() {
             }
         })
     }
+    $(document).keypress(function (e) {
+        var keyName = e.key;
+        console.log(keyName)
+
+        for(let i = 0; i < 10; i++) {
+             if (parseInt(keyName) === i) {
+                 if (opbStr === "0") {
+                     opbStr = i.toString()
+                     outputBox.text(opbStr)
+                     console.log("Is zero")
+                 } else {
+                     opbStr = opbStr + i.toString()
+                     outputBox.text(opbStr)
+                 }
+             }
+        }
+   })
 })
