e37c7f8e26ae0a3bc6c431a8b5647ee0441f61fe
aliennotfound <adamdreioe@gmail.com>
2022-09-06 08:10:20 +0200

    Save

--- jqScript.js
+++ jqScript.js
@@ -17,21 +17,102 @@ $(document).ready(function() {
             }
         })
     }
+
     $(document).keypress(function (e) {
         var keyName = e.key;
-        console.log(keyName)
+        var keyCode = e.code;
+        var p = ["NumpadAdd", "NumpadSubtract", "NumpadDivide", "NumpadMultiply"]
 
         for(let i = 0; i < 10; i++) {
              if (parseInt(keyName) === i) {
                  if (opbStr === "0") {
                      opbStr = i.toString()
                      outputBox.text(opbStr)
-                     console.log("Is zero")
                  } else {
                      opbStr = opbStr + i.toString()
                      outputBox.text(opbStr)
                  }
              }
         }
-   })
+
+        for(let i = 0; i < p.length; i++) {
+            if (keyCode === p[i]) {
+                if(opbStr.slice(-1) !== "+" &&
+                    opbStr.slice(-1) !== "-" &&
+                    opbStr.slice(-1) !== "*" &&
+                    opbStr.slice(-1) !== "/") {
+                    opbStr += keyName
+                    outputBox.text(opbStr)
+                }
+            }
+        }
+    })
+
+    $(document).keyup(function (e) {
+        var keyCode = e.code;
+
+        switch (keyCode) {
+            case "Backspace":
+                $.fn.bckspcInput();
+                break;
+
+            case "Delete":
+                $.fn.clearInput();
+                break;
+
+            case "Enter":
+                $.fn.equals();
+                break;
+
+            case "NumpadEnter":
+                $.fn.equals();
+                break;
+        }
+
+    })
 })
+
+$.fn.bckspcInput = function () {
+    opbStr = opbStr.slice(0, -1)
+    outputBox.text(opbStr)
+
+    if (opbStr <= 0) {
+        opbStr = "0"
+        outputBox.text(opbStr)
+    }
+}
+
+$.fn.clearInput = function() {
+    opbStr = "0"
+    outputBox.text(opbStr)
+}
+$.fn.operator = function(x) {
+    // var operatorBtn = [ 'plus', 'minus', 'divide', 'times1']
+    opbStr = x
+    outputBox.text(opbStr)
+}
+
+$.fn.equals = function () {
+    var op;
+    if(opbStr.includes("+"))
+    {
+        op = outputBox.text().split('+')
+        opbStr = parseInt(op[0]) + parseInt(op[1])
+        outputBox.text(opbStr)
+    } else if(opbStr.includes("-"))
+    {
+        op = outputBox.text().split('-')
+        opbStr = parseInt(op[0]) - parseInt(op[1])
+        outputBox.text(opbStr)
+    } else if(opbStr.includes("*"))
+    {
+        op = outputBox.text().split('*')
+        opbStr = parseInt(op[0]) * parseInt(op[1])
+        outputBox.text(opbStr)
+    } else if(opbStr.includes("/"))
+    {
+        op = outputBox.text().split('/')
+        opbStr = parseInt(op[0]) / parseInt(op[1])
+        outputBox.text(opbStr)
+    }
+}
